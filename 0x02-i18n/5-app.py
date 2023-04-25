6c0d47de543c76135f9ed8af9b2852c0b756117a
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-04-25 17:11:07 +0000

    4 5 and translations

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,8 @@
 '''Task 4: Force locale with URL parameter
 '''
 
-from flask import Flask, render_template, request
+from typing import Dict, Union
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -19,6 +20,30 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+
+    g.user = get_user()
 
 
 @babel.localeselector
@@ -39,7 +64,7 @@ def index() -> str:
     Returns:
         html: homepage
     '''
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 # uncomment this line and comment the @babel.localeselector
 # you get this error:
